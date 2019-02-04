77f745d957d0ecb446f974ff52ce3bfca308096c
Yuriy Yakovenko <yuriy.yakovenko@infopulse.com>
2019-02-04 23:32:48 +0200

    fix(integral to hex): added

--- src/Time.hs
+++ src/Time.hs
@@ -1,10 +1,14 @@
-module Time (hexTime) where
+module Time (hexTime, toHex) where
 
 import qualified Data.Time.Clock.POSIX as PT
 import Data.Hex (hex)
+import qualified Numeric as N
 
 data HexTime = HexTime String deriving Show
 
 -- get unix time in HEX format
 hexTime :: IO HexTime
-hexTime = fmap (HexTime . hex . show . round . flip (/) 30) PT.getPOSIXTime
+hexTime = fmap (HexTime . toHex . round . flip (/) 30) PT.getPOSIXTime
+
+toHex :: Integral a => a -> String
+toHex s = N.showHex (toInteger s) ""
