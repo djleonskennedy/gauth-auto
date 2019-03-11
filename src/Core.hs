24257eefaf7ca8ceea0e6b77aa41f1668a6c9e3c
Yuriy Yakovenko <Yuriy.Yakovenko@infopulse.com>
2019-03-11 15:53:52 +0200

    feat config generation

--- src/Core.hs
+++ src/Core.hs
@@ -2,18 +2,46 @@ module Core
   ( app
   ) where
 
-import File (Config, errorFileLoad, getConfig)
+import File (Config, secret, errorFileLoad, getConfig, writeConfig, getEmpty)
 import KeyManager (generate)
 import Encdec.Encoded
 import HuskPrelude
 import Time (getTime)
 import Encdec.Encoder as Encoder
 import Encdec.Encoding
+import Arguments (parse, extractConfigPath, extractTargetPath, flagToString, Flag( Help ))
+import System.Environment
+import Data.Maybe
+import Data.List
+import Data.Either
+import qualified Data.ByteString.Char8 as BChar8
 
 app :: IO ()
 app
  = do
-  let b32 = Encoded "EEEEAFFF44NNN5524QQQQUQMAUCI2222"
+  -- parse argument to specify config and file to save
+  (options, _) <- getArgs >>= parse
+
+  -- extracting config option
+  config <- getConfig . flagToString . fromMaybe Help $ find extractConfigPath options
+
+  -- log config
+  putStrLn $ "Config: " ++ show config
+
+  -- get message from time
   msg <- fmap Encoder.encode getTime :: IO (Encoded Hex)
+
+  --  get base 32 key from config
+  let b32 = either (\_ -> Encoded "")  (Encoded . BChar8.pack . secret) config
+
+  -- generate key
   key <- generate msg b32
-  print key
+
+  -- file path to save target file that will be used with any connect
+  let saveFilePath = flagToString . fromMaybe Help $ find extractTargetPath options
+
+  -- write cisco config
+  result <- writeConfig saveFilePath (fromRight getEmpty config) key
+
+  -- log it :)
+  print $ "Config: OK! ->" <> result
--- src/Time.hs
+++ src/Time.hs
@@ -1,7 +1,7 @@
 module Time (getTime) where
 
 import HuskPrelude
+import Data.Time.Clock.POSIX as PT
 
 getTime :: IO Int
---getTime = fmap (round . flip (/) 30) PT.getPOSIXTime
-getTime = return 51631600
+getTime = fmap (round . flip (/) 30) PT.getPOSIXTime
